c27add9171ea50a4255d214a675b59ba9588e0b4
vle <obentoka@gmail.com>
2020-03-12 13:54:11 -0400

    updated how pokemon was sorted

--- answers/Part4.sql
+++ answers/Part4.sql
@@ -1,12 +1,18 @@
 /*Part 4
-  ORDERED FROM HIGHEST LEVEL THEN PRIMARY TYPE THEN SUBTYPE DESCEDING.
-  CAME OUT WITH ARTICUNO AT THE TOP WHICH IS A LEGENDARY SO IM SATISFIED.*/
-  select
+  ORDERED FROM HIGHEST BASED ON ATT, SPA, SPD TOTAL.*/
+  select (attack + spatk + speed) as AttSpaSpd,
   t.trainername as trainer_name,
   p.name as pokemon_name,
   pt.pokelevel as level,
-  p.primary_type as type,
-  p.secondary_type as sub_type
-  from pokemons as p, pokemon_trainer as pt, trainers as t
-  where p.id = pt.pokemon_id and pt.trainerID = t.trainerID
-  order by level desc, type desc, sub_type desc nulls last;
+  ptype.name as type,
+  stype.name as sub_type
+  from pokemons as p
+  join pokemon_trainer as pt
+  on p.id = pt.pokemon_id
+  join trainers as t
+  on pt.trainerID = t.trainerID
+  right join types as ptype
+  on p.primary_type = ptype.id
+  left join types as stype
+  on p.secondary_type = stype.id
+  order by AttSpaSpd desc;
